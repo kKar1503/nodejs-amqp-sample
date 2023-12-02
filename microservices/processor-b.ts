d94a5cd782c463795df53565668dbfea16ab48c9
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2023-12-02 21:06:21 +0800

    feat: Added processorB microservice

---
+++ microservices/processor-b.ts
@@ -0,0 +1,75 @@
+import amqp from "amqplib";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(process.cwd(), ".env.processor-B") });
+
+const QUEUE = "processorB";
+const AMQP_URL = process.env["AMQP_CONN_URL"];
+
+async function main() {
+  if (!AMQP_URL) {
+    console.log("AMQP_URL cannot be empty!");
+    process.exit(1);
+  }
+
+  // using amqp.connect creates a connection directly to the amqp
+  const conn = await amqp.connect(AMQP_URL);
+
+  // channel is the way to connect to the queues to receive / send messages
+  const ch1 = await conn.createChannel();
+  // asserting a queue will indicate where the messages will come from
+  // as a microservice u will only need to care about which queue ur messages come from
+  // ideally, you should imagine each microservice has 1 queue.
+  // when expanding, we can create more instances of the same microservice and
+  // consume from the same queue
+  await ch1.assertQueue(QUEUE);
+
+  // Listener
+  ch1.consume(QUEUE, async (msg) => {
+    if (msg !== null) {
+      console.log(
+        "[%s] Received data from queue: %s",
+        new Date().toLocaleTimeString(),
+        msg.content.toString(),
+      );
+      // msg.content gives the buffer that is passed over by the amqp
+      const data = await handleTask(msg.content);
+      console.log(
+        "[%s] Task handled: %s",
+        new Date().toLocaleTimeString(),
+        data,
+      );
+      // ack will clear the task from the queue
+      // in the case where something goes wrong in this service, use nack
+      // nack basically means no acknowledge and will put the task back into the queue
+      // try change this to nack(msg) and observe what happens!
+      ch1.ack(msg);
+    } else {
+      console.log(
+        "[%s] Consumer cancelled by server",
+        new Date().toTimeString(),
+      );
+    }
+  });
+}
+
+main();
+
+interface Data {
+  durationInMs: number;
+  returnMsg: string;
+}
+
+async function handleTask(task: Buffer) {
+  // First we have to parse the buffer back to string
+  const json = task.toString();
+  // Then we can gracefully parse it back to whatever object
+  const obj: Data = JSON.parse(json);
+
+  return new Promise<string>((res) => {
+    setTimeout(() => {
+      res(obj.returnMsg.split("").reverse().join(""));
+    }, obj.durationInMs * 2);
+  });
+}
